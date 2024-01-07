ddcb3f177ee56f440b04311c9a8a945ccb09752b
Eash Aggarwal <eash268@gmail.com>
2024-01-06 22:50:34 -0500

    This is a quick push meant to get the branch ready to be checked out by someone else. Possible not working code.

---
+++ static/js/content.js
@@ -0,0 +1,103 @@
+var coding = `
+<div class="col l12 m12 s12 z-depth-0">
+  <div class="container">
+    <div class="row">
+      <div class="col l11 m11 s12 left-align">
+      
+        <div class="row">
+
+          <div class="col l4 m4 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/mantro.png" onclick="contactToTry()">
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An app that summarizes books and lets you interact with the author
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>
+
+          <div class="col l4 m4 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/serenademe3.png" onclick="contactToTry()">
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An app that creates Spotify playlists of recommended songs based on your likes
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>
+
+          <div class="col l4 m4 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/lucidia.png" onclick="contactToTry()">
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An app that texts you a daily quote of wisdom with accompanying art
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>
+
+        </div>
+
+        <div class="row" style="margin-top: 50px !important">
+
+          <div class="col l4 m4 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/periodica.png" onclick="contactToTry()">
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An app that lets you read news in the form of AI-generated summaries
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>
+
+          <div class="col l4 m4 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/lectura.png" onclick="contactToTry()">
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An app that uses AI to generate course notes for any subject
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>
+
+          <div class="col l4 m4 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/infinito.png" onclick="contactToTry()">
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An app that plays an infinite conversation between two AI bots
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>
+
+        </div>
+
+        <div class="row" style="margin-top: 50px !important">
+
+          <!--<div class="col l4 m4 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/plumo.png" onclick="contactToTry()">
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An app that analyzes your journal entries and gives you life advice
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>-->
+
+          <div class="col l4 m4 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/biblio.png" onclick="contactToTry()">
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An SMS-based version of ChatGPT
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>
+
+          <div class="col l4 m4 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/rmfl.png" onclick="contactToTry()">
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An app that roasts your fantasy football roster
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>
+
+          <div class="col l4 m4 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/rundown.png" onclick="contactToTry()">
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An app that gives you a complete rundown of any topic
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+`;
