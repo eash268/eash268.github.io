df7481b5389619ef9495a8c8a013039118f39e05
Eash Aggarwal <eash268@gmail.com>
2024-03-08 21:34:58 -0500

    added groknotes

--- static/js/content.js
+++ static/js/content.js
@@ -10,7 +10,7 @@ var coding = `
             <img class="pointer responsive-img" src="static/img/coding/mantro.png" 
               onclick="showModal(
                 'Mantro', 
-                '<b>Description:</b> After a deep conversation with a friend about mindfulness, he recommended a book which I really wanted to read, but I knew that I would probably buy it and start it but eventually get busy and never finish it. I thought it would be great if there was a way to get the key insights from the book without having to read the whole thing, so so I built Mantro, an app that summarizes books and lets you interact with the author. I used the Google Books API to scrape books then fed them to an LLM to generate a summary. I then added a feature where you can ask the author questions and the LLM will generate a response as if the author was responding. I added several more features such as book collections, summarizing multiple books at once, and searching books by topic. I built this as a PWA as a proof of concept but am currently working on rebuilding it in React Native.',
+                '<b>Description:</b> After a deep conversation with a friend about mindfulness, he recommended a book which I really wanted to read, but I knew that I would probably buy it and start it but eventually get busy and never finish it. I thought it would be great if there was a way to get the key insights from the book without having to read the whole thing, so so I built Mantro, an app that summarizes books and lets you interact with the author. I used the Google Books API to scrape books then fed them to an LLM to generate a summary. I then added a feature where you can ask the author questions and the LLM will generate a response as if the author was responding. I added several more features such as book collections, summarizing multiple books at once, and searching books by topic.',
                 '<b>Technologies:</b> Google Books API, OpenAI, Amazon Associates API, Flask, MongoDB', 
                 'static/img/coding/mantro.png',
                 ''
@@ -58,6 +58,26 @@ var coding = `
 
         <div class="row" style="margin-top: 50px !important">
 
+          <div class="col l12 m12 s12 minimargin">
+            <img class="pointer responsive-img" src="static/img/coding/groknotes.png" 
+              onclick="showModal(
+                'GrokNotes', 
+                '<b>Description:</b> ChatGPT is great to learn about topics at a high level, but to really get deep, you have to ask many follow-up questions. The best way to learn about a topic in detail is still to take a course on it, but courses are expensive and time-consuming. I built GrokNotes to solve this problem. GrokNotes is an app that uses AI to generate course notes for any subject. You can enter any topic and GrokNotes will generate a list of the most important concepts in that topic. You can then click on any concept to get detailed lecture notes for that concept. I also incorporated a feature that tests your understanding of concepts by asking you to type them in your own words and uses AI to check your answers. I encourage you to give it a try and hope you find it useful!', 
+                '<b>Technologies:</b> ChatGPT, OpenAI, Flask', 
+                'static/img/coding/groknotes2.png',
+                '<b>URL:</b> <a target=_blank href=https://groknotes.xyz>www.groknotes.xyz</a>'
+              )"
+            >
+            <p class="black-text nomargin menuitem left-align" style="width: 75%">
+              An app that uses AI to teach you anything
+            </p>
+            <br class="hide-on-med-and-up">
+          </div>
+
+        </div>
+
+        <div class="row" style="margin-top: 50px !important">
+
           <div class="col l4 m4 s12 minimargin">
             <img class="pointer responsive-img" src="static/img/coding/periodica.png" 
               onclick="showModal(
@@ -75,17 +95,17 @@ var coding = `
           </div>
 
           <div class="col l4 m4 s12 minimargin">
-            <img class="pointer responsive-img" src="static/img/coding/lectura.png" 
+            <img class="pointer responsive-img" src="static/img/coding/serenademe3.png" 
               onclick="showModal(
-                'Lectura', 
-                '<b>Description:</b> ChatGPT is great to learn about topics at a high level, but to really get deep, you have to ask many follow-up questions. The best way to learn about a topic in detail is still to take a course on it, but courses are expensive and time-consuming. I built Lectura to solve this problem. Lectura is an app that uses AI to generate course notes for any subject. You can enter any topic and Lectura will generate a list of the most important concepts in that topic. You can then click on any concept to get detailed lecture notes for that concept. I encourage you to give it a try and hope you find it useful!', 
-                '<b>Technologies:</b> ChatGPT, OpenAI, Flask', 
-                'static/img/coding/lectura.png',
-                '<b>URL:</b> <a target=_blank href=https://trylectura-85d083c6f86a.herokuapp.com>www.lectura.io</a>'
+                'SerenadeMe',
+                '<b>Description:</b> SerenadeMe is an AI that integrates with your Spotify account and creates playlists of recommended songs based on your likes. I built this app because I love listening to music but often find myself listening to the same songs over and over again. I wanted a way to discover new music that I would like, so I built SerenadeMe with a good friend. We registered the app on Spotify with the goal of sending it out to all our friends and family, but Spotify rejected our app because they do not allow developers to use any sort of machine learning or AI to develop on top of their API. Oh well! We still had a lot of fun building it and I have been using it to discover new music for myself. I have also been using it to discover new music for my friends and family by creating playlists for them and sending them the links. I have gotten great feedback from them and hope you enjoy it as well if it has not been taken down by Spotify by the time you use it!',
+                '<b>Technologies:</b> Google Books API, OpenAI, Amazon Associates API, Flask, MongoDB', 
+                'static/img/coding/serenademe3.png',
+                '<b>URL:</b> <a target=_blank href=https://serenademe.herokuapp.com>www.serenademe.io</a>'
               )"
             >
             <p class="black-text nomargin menuitem left-align" style="width: 75%">
-              An app that uses AI to generate course notes for any subject
+              An app that creates Spotify playlists of recommended songs based on your likes
             </p>
             <br class="hide-on-med-and-up">
           </div>
@@ -110,7 +130,7 @@ var coding = `
 
         <div class="row" style="margin-top: 50px !important">
 
-          <!--<div class="col l4 m4 s12 minimargin">
+          <div class="col l4 m4 s12 minimargin">
             <img class="pointer responsive-img" src="static/img/coding/plumo.png" 
               onclick="showModal(
                 'Mantro', 
@@ -124,7 +144,7 @@ var coding = `
               An app that analyzes your journal entries and gives you life advice
             </p>
             <br class="hide-on-med-and-up">
-          </div>-->
+          </div>
 
           <div class="col l4 m4 s12 minimargin">
             <img class="pointer responsive-img" src="static/img/coding/biblio.png" 
@@ -138,22 +158,6 @@ var coding = `
             >
             <p class="black-text nomargin menuitem left-align" style="width: 75%">
               An SMS-based version of ChatGPT
-            </p>
-            <br class="hide-on-med-and-up">
-          </div>
-
-          <div class="col l4 m4 s12 minimargin">
-            <img class="pointer responsive-img" src="static/img/coding/serenademe3.png" 
-              onclick="showModal(
-                'SerenadeMe',
-                '<b>Description:</b> SerenadeMe is an AI that integrates with your Spotify account and creates playlists of recommended songs based on your likes. I built this app because I love listening to music but often find myself listening to the same songs over and over again. I wanted a way to discover new music that I would like, so I built SerenadeMe with a good friend. We registered the app on Spotify with the goal of sending it out to all our friends and family, but Spotify rejected our app because they do not allow developers to use any sort of machine learning or AI to develop on top of their API. Oh well! We still had a lot of fun building it and I have been using it to discover new music for myself. I have also been using it to discover new music for my friends and family by creating playlists for them and sending them the links. I have gotten great feedback from them and hope you enjoy it as well if it has not been taken down by Spotify by the time you use it!',
-                '<b>Technologies:</b> Google Books API, OpenAI, Amazon Associates API, Flask, MongoDB', 
-                'static/img/coding/serenademe3.png',
-                '<b>URL:</b> <a target=_blank href=https://serenademe.herokuapp.com>www.serenademe.io</a>'
-              )"
-            >
-            <p class="black-text nomargin menuitem left-align" style="width: 75%">
-              An app that creates Spotify playlists of recommended songs based on your likes
             </p>
             <br class="hide-on-med-and-up">
           </div>
